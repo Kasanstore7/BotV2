2b1e60522d757a027c92ae05f5b9a18bab1ed12b
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-11-09 17:48:10 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `💌 Total Fitur saat ini: ${totalf}`,m)
+conn.reply(m.chat, `💀 Total Fitur XiaoSan Bot 💀: ${totalf}`,m)
 }
 
 handler.help = ['totalfitur']
